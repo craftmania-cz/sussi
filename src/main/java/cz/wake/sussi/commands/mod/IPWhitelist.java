c998193f88404053244e7f22669d0d71c4e9c44d
Speedy11CZ <michalspisak53@gmail.com>
2018-10-31 19:41:30 +0100

    Opravena gramatická chyba a chyba s pravomocemi

--- src/main/java/cz/wake/sussi/commands/mod/IPWhitelist.java
+++ src/main/java/cz/wake/sussi/commands/mod/IPWhitelist.java
@@ -20,77 +20,76 @@ public class IPWhitelist implements ICommand {
 
     @Override
     public void onCommand(User sender, MessageChannel channel, Message message, String[] args, Member member, EventWaiter w) {
-        if(!sender.getId().equals("238410025813540865") || !sender.getId().equals("177516608778928129")){
-            channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonáváni příkazu").setDescription("Na toto mají právo pouze Krosta a Kwak!").build()).queue();
-            return;
-        }
+        if(sender.getId().equals("238410025813540865") || sender.getId().equals("177516608778928129")) {
+            List<WhitelistedIP> ips = Sussi.getInstance().getSql().getWhitelistedIPs();
 
-        List<WhitelistedIP> ips = Sussi.getInstance().getSql().getWhitelistedIPs();
-
-        if(args.length < 1){
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setTitle("Seznam IP adres na whitelistu");
-            builder.setColor(Color.yellow);
-            for(WhitelistedIP ip : ips){
-                builder.appendDescription("**" + ip.getAddress() + "** - " + ip.getDescription() + "\n");
-            }
-            channel.sendMessage(builder.build()).queue();
-            return;
-        }
-
-        if(args[0].equals("add")){
-            if (args.length < 3) {
-                channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonáváni příkazu").setDescription("Nesprávně napsané argumenty! Př. ,ipwl add 1.1.1.1 Testovaci Zprava").build()).queue();
+            if(args.length < 1) {
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setTitle("Seznam IP adres na whitelistu");
+                builder.setColor(Color.yellow);
+                for(WhitelistedIP ip : ips) {
+                    builder.appendDescription("**" + ip.getAddress() + "** - " + ip.getDescription() + "\n");
+                }
+                channel.sendMessage(builder.build()).queue();
                 return;
             }
 
-            for (WhitelistedIP ip : Sussi.getInstance().getSql().getWhitelistedIPs()) {
-                if (args[1].equals(ip.getAddress())) {
-                    channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("IP Nalezena").setDescription("IP adresa " + args[1] + " již existuje.").build()).queue();
+            if(args[0].equals("add")) {
+                if(args.length < 3) {
+                    channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonávání příkazu").setDescription("Nesprávně napsané argumenty! Př. ,ipwl add 1.1.1.1 Testovaci Zprava").build()).queue();
                     return;
                 }
+
+                for(WhitelistedIP ip : Sussi.getInstance().getSql().getWhitelistedIPs()) {
+                    if(args[1].equals(ip.getAddress())) {
+                        channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("IP Nalezena").setDescription("IP adresa " + args[1] + " již existuje.").build()).queue();
+                        return;
+                    }
+                }
+
+                String description = args[2];
+
+                for(int i = 3; i != args.length; i++) {
+                    description += " " + args[i];
+                }
+
+                Sussi.getInstance().getSql().addWhitelistedIP(args[1], description);
+                channel.sendMessage(MessageUtils.getEmbed(Color.GREEN).setTitle("Příkaz byl úspěsně vykonán").setDescription("IP Adresa " + args[1] + " byla úspěšně přidána!").build()).queue();
             }
 
-            String description = args[2];
+            if(args[0].equals("remove")) {
+                if(args.length < 2) {
+                    channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonávání příkazu").setDescription("Nesprávně napsané argumenty! Př. ,ipwl remove 1.1.1.1").build()).queue();
+                    return;
+                }
 
-            for (int i = 3 ; i != args.length ; i++){
-                description += " " + args[i];
+                for(WhitelistedIP ip : Sussi.getInstance().getSql().getWhitelistedIPs()) {
+                    if(args[1].equals(ip.getAddress())) {
+                        Sussi.getInstance().getSql().removeWhitelistedIP(args[1]);
+                        channel.sendMessage(MessageUtils.getEmbed(Color.GREEN).setTitle("Příkaz byl úspěsně vykonán").setDescription("IP Adresa " + args[1] + " byla úspěšně odebrána!").build()).queue();
+                        return;
+                    }
+                }
+                channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("IP Nenalezena").setDescription("IP adresa " + args[1] + " nebyla nalezena.").build()).queue();
             }
 
-            Sussi.getInstance().getSql().addWhitelistedIP(args[1], description);
-            channel.sendMessage(MessageUtils.getEmbed(Color.GREEN).setTitle("Příkaz byl úspěsně vykonán").setDescription("IP Adresa " + args[1] + " byla úspěšně přidána!").build()).queue();
-        }
+            if(args[0].equals("check")) {
+                if(args.length < 2) {
+                    channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonávání příkazů").setDescription("Nesprávně napsané argumenty! Př. ,ipwl check 1.1.1.1").build()).queue();
+                    return;
+                }
 
-        if(args[0].equals("remove")){
-            if (args.length < 2) {
-                channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonáváni příkazu").setDescription("Nesprávně napsané argumenty! Př. ,ipwl remove 1.1.1.1").build()).queue();
+                for(WhitelistedIP ip : Sussi.getInstance().getSql().getWhitelistedIPs()) {
+                    if(args[1].equals(ip.getAddress())) {
+                        channel.sendMessage(MessageUtils.getEmbed(Color.yellow).setTitle("Informace o IP").setDescription("**" + ip.getAddress() + "** - " + ip.getDescription()).build()).queue();
+                        return;
+                    }
+                }
+                channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("IP Nenalezena").setDescription("IP adresa " + args[1] + " nebyla nalezena.").build()).queue();
                 return;
             }
-
-            for (WhitelistedIP ip : Sussi.getInstance().getSql().getWhitelistedIPs()) {
-                if (args[1].equals(ip.getAddress())) {
-                    Sussi.getInstance().getSql().removeWhitelistedIP(args[1]);
-                    channel.sendMessage(MessageUtils.getEmbed(Color.GREEN).setTitle("Příkaz byl úspěsně vykonán").setDescription("IP Adresa " + args[1] + " byla úspěšně odebrána!").build()).queue();
-                    return;
-                }
-            }
-            channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("IP Nenalezena").setDescription("IP adresa " + args[1] + " nebyla nalezena.").build()).queue();
-        }
-
-        if(args[0].equals("check")){
-            if(args.length < 2){
-               channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonávání příkazů").setDescription("Nesprávně napsané argumenty! Př. ,ipwl check 1.1.1.1").build()).queue();
-               return;
-            }
-
-            for(WhitelistedIP ip : Sussi.getInstance().getSql().getWhitelistedIPs()){
-                if(args[1].equals(ip.getAddress())){
-                    channel.sendMessage(MessageUtils.getEmbed(Color.yellow).setTitle("Informace o IP").setDescription("**" + ip.getAddress() + "** - " + ip.getDescription()).build()).queue();
-                    return;
-                }
-            }
-            channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("IP Nenalezena").setDescription("IP adresa " + args[1] + " nebyla nalezena.").build()).queue();
-            return;
+        } else {
+            channel.sendMessage(MessageUtils.getEmbed(Color.RED).setTitle("Chyba při vykonávání příkazu").setDescription("Na toto mají právo pouze Krosta a Kwak!").build()).queue();
         }
     }
 
