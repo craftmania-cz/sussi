c29d373e9e525f59489dc09def0b4896d5995303
Jacob Bordas <methalicity@gmail.com>
2019-07-23 20:50:42 +0200

    ,link

--- src/main/java/cz/wake/sussi/commands/CommandHandler.java
+++ src/main/java/cz/wake/sussi/commands/CommandHandler.java
@@ -59,7 +59,7 @@ public class CommandHandler {
         registerCommand(new Udrzba());
         registerCommand(new IPWhitelist());
         registerCommand(new NickWhitelist());
-        registerCommand(new Verify());
+        registerCommand(new Link());
         registerCommand(new Hytale());
         registerCommand(new Rust());
         registerCommand(new DeadbyDaylight());
