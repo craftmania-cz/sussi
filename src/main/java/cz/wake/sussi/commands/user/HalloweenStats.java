9e141fe45399b60f63fd58fcee5ebfa8234df03c
Jacob Bordas <methalicity@gmail.com>
2019-10-17 21:30:41 +0200

    Opravy linkovania
    xd

--- src/main/java/cz/wake/sussi/commands/user/HalloweenStats.java
+++ src/main/java/cz/wake/sussi/commands/user/HalloweenStats.java
@@ -21,11 +21,11 @@ public class HalloweenStats implements ICommand {
     public void onCommand(User sender, MessageChannel channel, Message message, String[] args, Member member, EventWaiter w) {
         String nick = "";
         if(args.length < 1) {
-            if (Sussi.getInstance().getSql().isAlreadyLinked(sender.getId())) {
-                nick = Sussi.getInstance().getSql().getLinkedNickname(sender.getId());
-            }
-            MessageUtils.sendErrorMessage("Špatně zadaný příkaz! Př. `,hl MrWakeCZ`", channel);
-            return;
+            if (!Sussi.getInstance().getSql().isAlreadyLinked(sender.getId())) {
+                MessageUtils.sendErrorMessage("Špatně zadaný příkaz! Př. `,hl MrWakeCZ`", channel);
+                return;
+            } else nick = Sussi.getInstance().getSql().getLinkedNickname(sender.getId());
+
         }
         if (nick == "") nick = args[0];
 
