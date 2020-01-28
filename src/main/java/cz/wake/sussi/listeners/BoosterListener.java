aa618a30e7da6a1949dfab93b13be59b589bffba
Speedy11CZ <michalspisak53@gmail.com>
2020-01-28 12:24:20 +0100

    Vypnuto odesílání zprávy o boostingu při vypršení

--- src/main/java/cz/wake/sussi/listeners/BoosterListener.java
+++ src/main/java/cz/wake/sussi/listeners/BoosterListener.java
@@ -19,13 +19,15 @@ public class BoosterListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberUpdateBoostTime(GuildMemberUpdateBoostTimeEvent e) {
-        Member member = e.getMember();
-        member.getUser().openPrivateChannel().queue(msg -> msg.sendMessage(
-                MessageUtils.getEmbed(new Color(255, 80, 255))
-                .setTitle("Děkujeme za Boost CraftManie! " + EmoteList.ZABICKA_BOOSTER)
-                .setDescription("Ahoj, tímto bychom ti chtěli poděkovat za Boost, který jsi dal(a) našemu Discord serveru. Tímto se ti také odemkly výhody u nás na Discordu a brzo také i na Minecraft serveru.")
-                .addField("Seznam výhod", "- Booster role + růžový prefix\n- Změna barvy nicku pomocí `,booster color`", false)
-                .setFooter("Kdo nebůstí s námi, bůstí proti nám!")
-                .setThumbnail("https://discordapp.com/assets/b941bc1dfe379db6cc1f2acc5a612f41.png").build()).queue());
+        if(e.getNewTimeBoosted() != null) {
+            Member member = e.getMember();
+            member.getUser().openPrivateChannel().queue(msg -> msg.sendMessage(
+                    MessageUtils.getEmbed(new Color(255, 80, 255))
+                            .setTitle("Děkujeme za Boost CraftManie! " + EmoteList.ZABICKA_BOOSTER)
+                            .setDescription("Ahoj, tímto bychom ti chtěli poděkovat za Boost, který jsi dal(a) našemu Discord serveru. Tímto se ti také odemkly výhody u nás na Discordu a brzo také i na Minecraft serveru.")
+                            .addField("Seznam výhod", "- Booster role + růžový prefix\n- Změna barvy nicku pomocí `,booster color`", false)
+                            .setFooter("Kdo nebůstí s námi, bůstí proti nám!")
+                            .setThumbnail("https://discordapp.com/assets/b941bc1dfe379db6cc1f2acc5a612f41.png").build()).queue());
+        }
     }
 }
