2cf117c2996467ddb79fd62bafc17901084b544d
MrWakeCZ <joseff.kral@gmail.com>
2020-04-14 13:37:56 +0200

    Deaktivace oznamování boosterům

--- src/main/java/cz/wake/sussi/listeners/BoosterListener.java
+++ src/main/java/cz/wake/sussi/listeners/BoosterListener.java
@@ -1,25 +1,16 @@
 package cz.wake.sussi.listeners;
 
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-import cz.wake.sussi.utils.EmoteList;
-import cz.wake.sussi.utils.MessageUtils;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.awt.*;
-
 public class BoosterListener extends ListenerAdapter {
-
-    private EventWaiter w;
-
-    public BoosterListener(EventWaiter w) {
-        this.w = w;
-    }
 
     @Override
     public void onGuildMemberUpdateBoostTime(GuildMemberUpdateBoostTimeEvent e) {
-        if(e.getNewTimeBoosted() != null && e.getOldTimeBoosted() == null) {
+        Member member = e.getMember();
+        System.out.println("[BOOST]: " + member.getNickname() + " - newTime(" + e.getNewTimeBoosted() + "), oldTime(" + e.getOldTimeBoosted() + ")");
+        /*if(e.getNewTimeBoosted() != null && e.getOldTimeBoosted() == null) {
             Member member = e.getMember();
             member.getUser().openPrivateChannel().queue(msg -> msg.sendMessage(
                     MessageUtils.getEmbed(new Color(255, 80, 255))
@@ -28,6 +19,6 @@ public class BoosterListener extends ListenerAdapter {
                             .addField("Seznam výhod", "- Booster role + růžový prefix\n- Změna barvy nicku pomocí `,booster color`", false)
                             .setFooter("Kdo nebůstí s námi, bůstí proti nám!")
                             .setThumbnail("https://discordapp.com/assets/b941bc1dfe379db6cc1f2acc5a612f41.png").build()).queue());
-        }
+        }*/
     }
 }
