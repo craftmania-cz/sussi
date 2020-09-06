98efe70db39a8e5ae5307999205d4972a53795c9
Sergey Kuroedov <sergej.kuroedov@student.uzlabina.cz>
2020-09-06 21:15:03 +0200

    Přidány hlášky k sub-příkazům

--- src/main/java/cz/wake/sussi/commands/user/Room.java
+++ src/main/java/cz/wake/sussi/commands/user/Room.java
@@ -10,6 +10,9 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Room implements ICommand {
     @Override
     public void onCommand(User sender, MessageChannel channel, Message message, String[] args, Member member, EventWaiter w) {
@@ -17,58 +20,59 @@ public class Room implements ICommand {
 
 
         if (Sussi.getInstance().getSql().getPlayerVoiceRoomIdByOwnerId(sender.getIdLong()) == 0) {
-            //TODO: error hláška - nemáš žádnou room
+            MessageUtils.sendErrorMessage("Nemáš žádnou místnost! Vytvoříš si ji připojením do kanálu <room>", channel);
         }
 
         VoiceChannel voiceChannel = member.getGuild().getVoiceChannelById(Sussi.getInstance().getSql().getPlayerVoiceRoomIdByOwnerId(sender.getIdLong()));
 
         if (args.length < 1) {
-            //TODO: room info
-            EmbedBuilder embedBuilder = new EmbedBuilder()
+            boolean locked = !voiceChannel.getPermissionOverride(member.getGuild().getPublicRole()).getAllowed().contains(Permission.VOICE_CONNECT);
+            EmbedBuilder embedBuilder = new EmbedBuilder()             //TODO: Banned, added people
                     .setTitle("Informace o voice místnosti uživatele " + sender.getAsTag())
                     .addField("Základní informace",
                             "Název: " + voiceChannel.getName() + "\n" +
-                                    "Limit: " + voiceChannel.getUserLimit() + "\n" + //TODO: if unlocked => bez limitu
+                                    "Zamknuto: " + (locked ? "Ano" : "Ne") + "\n" +
+                                    "Limit: " + (voiceChannel.getUserLimit() == 0 ? "Bez limitu" : voiceChannel.getUserLimit()) + "\n" +
                                     "Bitrate: " + voiceChannel.getBitrate(), false);
             channel.sendMessage(embedBuilder.build()).queue();
         } else {
             switch (args[0]) {
                 case "lock":
-                    voiceChannel.getManager().getChannel().putPermissionOverride(member.getGuild().getPublicRole()).setDeny(Permission.VOICE_CONNECT).queue();
+                    voiceChannel.putPermissionOverride(member.getGuild().getPublicRole()).setDeny(Permission.VOICE_CONNECT).queue();
                     MessageUtils.sendAutoDeletedMessage(member.getAsMention() + " tvá místnost byla zamknuta.", 3000, channel);
                     break;
                 case "unlock":
-                    voiceChannel.getManager().getChannel().putPermissionOverride(member.getGuild().getPublicRole()).setAllow(Permission.VOICE_CONNECT).queue();
+                    voiceChannel.putPermissionOverride(member.getGuild().getPublicRole()).setAllow(Permission.VOICE_CONNECT).queue();
                     MessageUtils.sendAutoDeletedMessage(member.getAsMention() + " tvá místnost byla odemknuta", 3000, channel);
                     break;
                 case "add":
                     if (args.length == 2 || message.getMentions(Message.MentionType.USER).size() > 0) {
                         Member toAdd = member.getGuild().getMemberById(message.getMentions(Message.MentionType.USER).get(0).getId());
                         voiceChannel.getManager().getChannel().createPermissionOverride(toAdd).setAllow(Permission.VOICE_CONNECT).queue();
-                        //TODO: úspěch hláška
+                        MessageUtils.sendAutoDeletedMessage(member.getAsMention() + " uživatel byl přidán.", 3000, channel);
                     } else {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Musíš označit uživatele, kterého chceš přidat!", channel);
                     }
                     break;
                 case "remove":
                     if (args.length == 2 || message.getMentions(Message.MentionType.USER).size() > 0) {
                         Member toAdd = member.getGuild().getMemberById(message.getMentions(Message.MentionType.USER).get(0).getId());
                         voiceChannel.getManager().getChannel().createPermissionOverride(toAdd).setDeny(Permission.VOICE_CONNECT).queue();
-                        //TODO: úspěch hláška
+                        MessageUtils.sendAutoDeletedMessage("Uživatel byl odebrán.", 3000, channel);
                     } else {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Musíš označit uživatele, kterého chceš odebrat!", channel);
                     }
                     break;
                 case "limit":
                     try {
                         if (args.length == 2) {
                             voiceChannel.getManager().setUserLimit(Integer.parseInt(args[1])).queue();
-                            //TODO: úspěch hláška
+                            MessageUtils.sendAutoDeletedMessage("Limit byl změněn na " + args[1], 3000, channel);
                         } else {
                             throw new Exception();
                         }
                     } catch (Exception e) {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Limit je ve špatném formátu!", channel);
                     }
                     break;
                 case "kick":
@@ -76,12 +80,12 @@ public class Room implements ICommand {
                         Member toKick = member.getGuild().getMemberById(message.getMentions(Message.MentionType.USER).get(0).getId());
                         if (toKick.getVoiceState().inVoiceChannel() && toKick.getVoiceState().getChannel().getId().equals(voiceChannel.getId())) {
                             voiceChannel.getGuild().kickVoiceMember(toKick).queue();
-                            //TODO: úspěch hláška
+                            MessageUtils.sendAutoDeletedMessage("Uživatel byl vykopnut.", 3000, channel);
                         } else {
-                            //TODO: error hláška - uživatel není v (stejném) kanálu
+                            MessageUtils.sendErrorMessage("Uživatel není ve stejném kanálu", channel);
                         }
                     } else {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Musíš označit uživatele, kterého chceš vykopnout!", channel);
                     }
                     break;
                 case "ban":
@@ -89,30 +93,33 @@ public class Room implements ICommand {
                         Member toBan = member.getGuild().getMemberById(message.getMentions(Message.MentionType.USER).get(0).getId());
                         if (toBan.getVoiceState().inVoiceChannel() && toBan.getVoiceState().getChannel().getId().equals(voiceChannel.getId())) {
                             voiceChannel.getGuild().kickVoiceMember(toBan).queue();
-                            //TODO: úspěch hláška
+                            MessageUtils.sendAutoDeletedMessage("Uživatel byl zabanován.", 3000, channel);
                         }
                         voiceChannel.getManager().getChannel().createPermissionOverride(toBan).setDeny(Permission.VIEW_CHANNEL).queue();
                     } else {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Musíš označit uživatele, kterého chceš zabanovat!", channel);
                     }
                 case "bitrate":
                     try {
                         if (args.length == 2) {
                             voiceChannel.getManager().setBitrate(Integer.parseInt(args[1])).queue();
-                            //TODO: úspěch hláška
+                            MessageUtils.sendAutoDeletedMessage("Bitrate byl změněn.", 3000, channel);
                         } else {
                             throw new Exception();
                         }
                     } catch (Exception e) {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Bitrate je ve špatném formátu!", channel);
                     }
                     break;
                 case "name":
-                    if (args.length == 2) {
-                        voiceChannel.getManager().setName(args[1]).queue();
-                        //TODO: úspěch hláška
+                    if (args.length >= 1) {
+                        List<String> list = Arrays.asList(args);
+                        list = list.subList(1, list.size());
+                        String name = String.join(" ", list);
+                        voiceChannel.getManager().setName(name).queue();
+                        MessageUtils.sendAutoDeletedMessage("Název byl změněn.", 3000, channel);
                     } else {
-                        //TODO: error hláška
+                        MessageUtils.sendErrorMessage("Musíš napsat uvést název místnosti!", channel);
                     }
                     break;
             }
