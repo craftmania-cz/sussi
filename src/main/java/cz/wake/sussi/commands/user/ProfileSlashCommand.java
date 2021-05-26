544a9f0ffc87edeb9e04bcf701f934cd2c4b7974
MrWakeCZ <joseff.kral@gmail.com>
2021-05-27 01:43:15 +0200

    Odkaz na profil v `/profil`

--- src/main/java/cz/wake/sussi/commands/user/ProfileSlashCommand.java
+++ src/main/java/cz/wake/sussi/commands/user/ProfileSlashCommand.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.button.Button;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.apache.commons.lang3.StringUtils;
 
@@ -152,7 +153,7 @@ public class ProfileSlashCommand implements ISlashCommand {
                     true);
         }
 
-        hook.sendMessageEmbeds(embedBuilder.build()).queue();
+        hook.sendMessageEmbeds(embedBuilder.build()).addActionRow(Button.link("https://stats.craftmania.cz/player/" + profile.getName(), "Profil na Stats")).queue();
     }
 
     private Color getColorByRank(int rank){
